a1f1097490c3d887a71a7ad522d9fe3beaa62400
Gudang Coding <dientrimedia@gmail.com>
2019-03-20 02:12:11 +0700

    Integrasi Dengan AdminLTE

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  
+  <link rel="stylesheet" href="{{ asset('public/adminLTE/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('public/font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('public/adminLTE/dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('public/adminLTE/dist/css/skins/skin-blue.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('public/adminLTE/plugins/datatables/dataTables.bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ asset('public/adminLTE/plugins/datepicker/datepicker3.css') }}">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+   <!-- Header -->
+  <header class="main-header">
+
+    <a href="#" class="logo">
+      <span class="logo-mini"><b>Atomic</b></span>
+     <span class="logo-lg"><b>Atomid</b>ID</span>
+    </a>
+
+
+    <nav class="navbar navbar-static-top" role="navigation">
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      
+      <div class="navbar-custom-menu">
+        
+      </div>
+    </nav>
+  </header>
+  <!-- End Header -->
+
+
+  <!-- Sidebar -->
+  <aside class="main-sidebar">
+
+    <section class="sidebar">
+      <ul class="sidebar-menu">
+        <li class="header">MENU NAVIGASI</li>
+
+        <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+
+         <li class="treeview">
+          <a href="#">
+            <i class="fa fa-list"></i> <span>Master</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Dompet</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Kategori</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-list"></i> <span>Tansaksi</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Dompet Masuk</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Dompet Keluar</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-list"></i> <span>Laporan</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#"><i class="fa fa-circle-o"></i> Laporan Transaksi</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+  </aside>
+  <!-- End Sidebar -->
+
+  <!-- Content  -->
+  <div class="content-wrapper">
+   <section class="content-header">
+      <h1>
+        @yield('title')
+      </h1>
+      <ol class="breadcrumb">
+        @section('breadcrumb')
+        <li><a href="#"><i class="fa fa-home"></i>Home</a></li>
+        @show
+      </ol>
+    </section>
+
+    <section class="content">
+        @yield('content')
+    </section>
+  </div>
+  <!-- End Content -->
+
+  <!-- Footer -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      Maju Bersama Atomic Indonesia
+    </div>
+    <strong>Copyright &copy; 2019 <a href="#">Company</a>.</strong> All rights reserved.
+  </footer>
+  <!-- End Footer -->
+ 
+<script src="{{ asset('public/adminLTE/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+<script src="{{ asset('public/adminLTE/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/adminLTE/dist/js/app.min.js') }}"></script>
+
+<script src="{{ asset('public/adminLTE/plugins/chartjs/Chart.min.js') }}"></script>
+<script src="{{ asset('public/adminLTE/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('public/adminLTE/plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
+<script src="{{ asset('public/adminLTE/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
+<script src="{{ asset('public/js/validator.min.js') }}"></script>
+
+@yield('script')
+
+</body>
+</html>
